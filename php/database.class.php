1586e6f417b2894c531ce870b318dcaccd0a94bd
Harry <harry.pearson@student.anglia.ac.uk>
2018-10-20 07:48:08 -0700

    add database class

--- php/database.class.php
+++ php/database.class.php
@@ -1,6 +1,26 @@
 <?php
 
+class Database{
 
+	private $connection;
+	public function __construct($DSN, $USR, $PWD)
+	{
+		try
+		{
+			$this->connection = new PDO(DSN, USR, PWD);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO nametable (firstname, lastname) VALUES ('yay', 'Bucke')";
+			$this->connection->exec($sql);
+		}
+
+		catch(PDOException $e)
+		{
+			echo "FAILED";
+			echo $e;
+		}
+
+	}
+}
 
 
 ?>
--- start.php
+++ start.php
@@ -2,21 +2,7 @@
 require './php/config.php';
 require './php/database.class.php';
 
-try
-{
+$database = new Database(DSN, USR, PWD);
 
-	$connection = new PDO(DSN, USR, PWD);
-	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "CONNECTED OK";
-
-	$sql = "INSERT INTO nametable (firstname, lastname) VALUES ('Bob', 'Bucke')";
-	$connection->exec($sql);
-}
-
-catch(PDOException $e)
-{
-	echo "FAILED";
-	echo $e;
-}
 
 ?>
